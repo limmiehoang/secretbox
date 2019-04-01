370ce77109e55fef4d6acdfae26583fa3134d113
Khanh Hoang <minhkhanhhoang2105@gmail.com>
2019-04-01 23:22:05 +0700

    fix(HomepageUI): modify UI

--- secretbox-fe/src/views/Home.vue
+++ secretbox-fe/src/views/Home.vue
@@ -2,11 +2,16 @@
   <div class="home">
     <div class="sidebar-wrapper">
       <div class="sidebar-header">
-        <router-link to="/">SecretBox</router-link>
+        <router-link to="/">
+          <img
+            src="../assets/logo.png"
+            alt="SecretBox">
+          <span>SecretBox</span>
+        </router-link>
       </div>
       <div class="sidebar">
         <div class="sidebar-button">
-          <button class="btn btn-outline-secondary btn-sm">New Group</button>
+          <button class="btn btn-outline-dark btn-sm">New Group</button>
         </div>
         <div class="sidebar-nav">
           <div class="sidebar-nav-item">
@@ -34,8 +39,8 @@
         <div class="profile">
           <b-dropdown id="ddown-right" right variant="link" size="sm" no-caret>
             <template slot="button-content">
-              <img :src="profile.picture" alt="Avatar" class="avatar" onError="this.onerror=null;this.src='https://i.pinimg.com/236x/40/41/fa/4041faa0a8989e5787f9b164ca3b2650--occupational-therapist-physical-therapist.jpg';">
               <a href="">{{ profile.name }}</a>
+              <img :src="profile.picture" alt="Avatar" class="avatar" onError="this.onerror=null;this.src='https://i.pinimg.com/236x/40/41/fa/4041faa0a8989e5787f9b164ca3b2650--occupational-therapist-physical-therapist.jpg';">
             </template>
             <b-dropdown-item @click.prevent="logout">Logout</b-dropdown-item>
           </b-dropdown>
@@ -79,7 +84,7 @@
           </table>
         </div>
         <div class="info-banner">
-          <button class="btn btn-primary btn-sm">Upload</button>
+          <button class="btn btn-sm">Upload</button>
         </div>
       </div>
     </div>
@@ -131,18 +136,40 @@ export default {
   background-color: #F7F9FA;
 }
 
-.sidebar-wrapper .sidebar-header {
+.sidebar-header,
+.nav-bar {
+  height: 66px;
+}
+
+.sidebar-wrapper .sidebar-header,
+.sidebar-nav {
   box-sizing: border-box;
-  height: 50px;
   display: flex;
-  justify-content: center;
-  padding: 8px;
+  flex-direction: column;
+  text-align: left;
+  justify-content: flex-start;
+  padding: 16px 40px;
   width: 100%;
 }
 
-.sidebar-wrapper .sidebar-header a {
+.sidebar-header a {
   line-height: 34px;
+  display: flex;
+}
+
+.sidebar-header span {
+  line-height: 34px;
+  display: inline-block;
   font-size: 20px;
+  font-weight: 600;
+  opacity: .84;
+  margin-left: 15px;
+  color: #333;
+}
+
+.sidebar-header img {
+  width: 34px;
+  height: 34px;
 }
 
 .sidebar-wrapper .sidebar {
@@ -150,17 +177,14 @@ export default {
   position: relative;
 }
 
-.sidebar-button {
-  padding: 20px 50px;
+.sidebar-button,
+.info-banner {
+  padding: 20px 40px;
 }
 
 .sidebar-button button {
   width: 100%;
-}
-
-.sidebar-nav {
-  padding: 8px 34px;
-  text-align: left;
+  font-weight: 600;
 }
 
 .sidebar-nav-item>a {
@@ -189,21 +213,26 @@ export default {
 
 .page-content .nav-bar {
   box-sizing: border-box;
-  height: 50px;
-  padding: 8px;
   position: relative;
   z-index: 201;
 }
 
 .nav-bar .profile {
   position: absolute;
-  right: 10px;
+  right: 32px;
+  top: 0;
+  bottom: 0;
+  display: flex;
+}
+
+.profile a {
+  font-weight: 600;
+  opacity: .84;
 }
 
 .page-content .main-content {
   display: flex;
   flex: 1 1 0%;
-  padding: 20px;
 }
 
 .main-content .presentation {
@@ -212,21 +241,24 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
+  padding-left: 40px;
 }
 
 .presentation-breadcrumb {
-  height: 50px;
+  height: 40px;
 }
 
 .main-content .info-banner {
   flex: 0 2 25%;
   max-width: 420px;
   min-width: 200px;
-  padding: 20px 50px;
 }
 
 .info-banner button {
   width: 100%;
+  background-color: #e76123;
+  color: #fff;
+  font-weight: 600;
 }
 
 .avatar {
@@ -234,6 +266,6 @@ export default {
   width: 30px;
   height: 30px;
   border-radius: 50%;
-  margin-right: 10px;
+  margin-left: 10px;
 }
 </style>
