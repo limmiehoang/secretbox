4799c3a6dad422ce7080e78c69ed34cb5486c695
Khanh Hoang <minhkhanhhoang2105@gmail.com>
2019-04-17 21:43:17 +0700

    fix(Model): change to random id string model

--- secretbox-be/app/EncFile.php
+++ secretbox-be/app/EncFile.php
@@ -4,21 +4,17 @@ namespace SecretBox;
 
 use Illuminate\Database\Eloquent\Model;
 
-class File extends RandomIdModel
+class EncFile extends Model
 {
     // Using a non-incrementing and non-numeric primary key
     public $incrementing = false;
     protected $keyType = 'string';
+    public $timestamps = false;
 
 
     protected $fillable = [
-        'name',  'type', 'extension', 'user_id', 'group_id', 'url'
+        'group_id', 'enc_metadata'
     ];
-
-    public function user()
-    {
-        return $this->belongsTo('SecretBox\User');
-    }
 
     public function group()
     {
--- secretbox-be/app/Group.php
+++ secretbox-be/app/Group.php
@@ -4,9 +4,39 @@ namespace SecretBox;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Group extends Model
+class Group extends RandomIdModel
 {
+    // Using a non-incrementing and non-numeric primary key
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+
     protected $fillable = [
-        'name'
+        'name', 'initial_data'
     ];
+
+    public function files()
+    {
+        return $this->hasMany('SecretBox\File');
+    }
+
+    public function encFiles()
+    {
+        return $this->hasMany('SecretBox\EncFile');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany('SecretBox\User');
+    }
+
+    public function initialUser()
+    {
+        return $this->belongsTo('SecretBox\User');
+    }
+
+    public function initialData()
+    {
+        return $this->belongsTo('SecretBox\EncFile');
+    }
 }
