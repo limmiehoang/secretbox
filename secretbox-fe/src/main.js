544cc58602cb2c0fd7515409938dcaba3784b967
Khanh Hoang <minhkhanhhoang2105@gmail.com>
2019-05-12 22:33:20 +0700

    add websocket service

--- secretbox-fe/src/main.js
+++ secretbox-fe/src/main.js
@@ -6,11 +6,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import VueResource from "vue-resource"
+import BootstrapVue from 'bootstrap-vue';
+
 import AuthPlugin from "./plugins/auth";
 import CryptoPlugin from "./plugins/crypto";
 import HelperPlugin from "./plugins/helpers";
-import BootstrapVue from 'bootstrap-vue';
+import WSPlugin from "./plugins/ws";
 
 Vue.use(VueResource);
 Vue.use(BootstrapVue);
@@ -18,6 +21,7 @@ Vue.use(BootstrapVue);
 Vue.use(AuthPlugin);
 Vue.use(CryptoPlugin);
 Vue.use(HelperPlugin);
+Vue.use(WSPlugin);
 
 Vue.config.productionTip = false;
 Vue.http.options.root = 'http://localhost:8000';
